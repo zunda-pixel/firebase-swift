1d6f4dc1c8936db9598667fb59230c1abc69e31a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-10 03:49:35 +0900

    add User parameters

--- Sources/Auth/ProviderUserInfo.swift
+++ Sources/Auth/ProviderUserInfo.swift
@@ -1,6 +1,15 @@
+import Foundation
+
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
 public struct ProviderUserInfo: Sendable, Hashable, Codable {
   public var providerId: String
   public var federatedId: String
   public var email: String
   public var rawId: String
+  public var displayName: String?
+  public var photoUrl: URL?
+  public var screenName: String?
 }
