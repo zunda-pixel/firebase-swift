21fca5e830f3eb0dbe6c6d16a750f05f17eb0530
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-10 05:32:14 +0900

    make email optional

--- Sources/Auth/ProviderUserInfo.swift
+++ Sources/Auth/ProviderUserInfo.swift
@@ -7,7 +7,7 @@ import FoundationNetworking
 public struct ProviderUserInfo: Sendable, Hashable, Codable {
   public var providerId: String
   public var federatedId: String
-  public var email: String
+  public var email: String?
   public var rawId: String
   public var displayName: String?
   public var photoUrl: URL?
