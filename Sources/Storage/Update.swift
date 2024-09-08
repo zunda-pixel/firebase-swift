e6fba050b226a84c159c2c0ca46a5b4eff73db52
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-09-08 23:33:17 +0900

    add update

---
+++ Sources/Storage/Update.swift
@@ -0,0 +1,92 @@
+import Foundation
+import HTTPTypes
+
+private struct Body: Codable, Sendable, Hashable {
+  var cacheControl: String?
+  var contentDisposition: String?
+  var contentEncoding: String?
+  var contentLanguage: String?
+  var contentType: String?
+  var customMetadata: [String: String]?
+  
+  private enum CodingKeys: String, CodingKey {
+    case cacheControl
+    case contentDisposition
+    case contentEncoding
+    case contentLanguage
+    case contentType
+    case customMetadata = "metadata"
+  }
+}
+
+extension Storage {
+  @discardableResult
+  public func update(
+    bucket: String,
+    filePath: String,
+    md5Hash: String? = nil,
+    cacheControl: String? = nil,
+    contentDisposition: String? = nil,
+    contentEncoding: String? = nil,
+    contentLanguage: String? = nil,
+    contentType: String? = nil,
+    customMetadata: [String: String]? = nil
+  ) async throws -> Item {
+    let body = Body(
+      cacheControl: cacheControl,
+      contentDisposition: contentDisposition,
+      contentEncoding: contentEncoding,
+      contentLanguage: contentLanguage,
+      contentType: contentType,
+      customMetadata: customMetadata
+    )
+        
+    let bodyData = try! JSONEncoder().encode(body)
+    
+    let bucket = percentEncode(bucket)
+    let filePath = percentEncode(filePath)
+    let path = "v0/b/\(bucket)/o/\(filePath)"
+
+    let endpoint = URL(string: "\(baseUrl.absoluteString)\(path)")!
+
+    let request = HTTPRequest(
+      method: .patch,
+      url: endpoint,
+      headerFields: [
+        .contentType: "application/json; charset=UTF-8",
+        .contentLength: bodyData.count.description,
+      ]
+    )
+    
+    let (data, _) = try await httpClient.execute(for: request, from: bodyData)
+
+    let item = try JSONDecoder().decode(Item.self, from: data)
+    
+    return item
+  }
+}
+
+extension Storage {
+  @discardableResult
+  public func update(
+    item: Item,
+    md5Hash: String? = nil,
+    cacheControl: String? = nil,
+    contentDisposition: String? = nil,
+    contentEncoding: String? = nil,
+    contentLanguage: String? = nil,
+    contentType: String? = nil,
+    customMetadata: [String: String]? = nil
+  ) async throws -> Item {
+    try await self.update(
+      bucket: item.bucket,
+      filePath: item.name,
+      cacheControl: cacheControl,
+      contentDisposition: contentDisposition,
+      contentEncoding: contentEncoding,
+      contentLanguage: contentLanguage,
+      contentType: contentType,
+      customMetadata: customMetadata
+    )
+  }
+}
