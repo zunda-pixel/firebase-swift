72e571feba8ddad3581046c880cd0a656784ef47
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-09-08 23:32:51 +0900

    fix name

--- Sources/Storage/Upload.swift
+++ Sources/Storage/Upload.swift
@@ -2,18 +2,20 @@ import Foundation
 import HTTPTypes
 
 extension Storage {
+  @discardableResult
   public func upload(
-    backetName: String,
+    bucket: String,
     filePath: String,
     data: Data,
     contentType: String
   ) async throws -> Item {
+    let bucket = percentEncode(bucket)
     let filePath = percentEncode(filePath)
     let queries = [
       "uploadType": "resumable",
       "name": filePath,
     ]
-    let path = "v0/b/\(backetName)/o/\(filePath)?\(queries.map { [$0.key, $0.value].joined(separator: "=") }.joined(separator: "&"))"
+    let path = "v0/b/\(bucket)/o/\(filePath)?\(queries.map { [$0.key, $0.value].joined(separator: "=") }.joined(separator: "&"))"
 
     let endpoint = URL(string: "\(baseUrl.absoluteString)\(path)")!
 
