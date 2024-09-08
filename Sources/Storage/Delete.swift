04c4190467768a3736fc3c1f25e127546781f897
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-09-09 00:43:46 +0900

    add delete

---
+++ Sources/Storage/Delete.swift
@@ -0,0 +1,36 @@
+import Foundation
+import HTTPTypes
+
+extension Storage {
+  public func delete(
+    bucket: String,
+    filePath: String
+  ) async throws {
+    let bucket = percentEncode(bucket)
+    let filePath = percentEncode(filePath)
+    let path = "v0/b/\(bucket)/o/\(filePath)"
+
+    let endpoint = URL(string: "\(baseUrl.absoluteString)\(path)")!
+
+    let request = HTTPRequest(
+      method: .delete,
+      url: endpoint
+    )
+    
+    let (data, response) = try await httpClient.execute(for: request, from: nil)
+    
+    if response.status.code != 204 {
+      let response = try JSONDecoder().decode(ErrorsResponse.self, from: data)
+      throw response.error
+    }
+  }
+}
+
+extension Storage {
+  public func delete(item: Item) async throws {
+    try await self.delete(
+      bucket: item.bucket,
+      filePath: item.name
+    )
+  }
+}
