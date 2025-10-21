d4973195caa702667021f474793258c80cd10efb
zunda <47569369+zunda-pixel@users.noreply.github.com>
2025-10-21 18:58:52 +0900

    fix test (#50)
    * fix test
    
    * fix code
    
    * add import FoundationNetworking

--- Tests/RemoteConfigTests/RemoteConfigTests.swift
+++ Tests/RemoteConfigTests/RemoteConfigTests.swift
@@ -3,39 +3,50 @@ import HTTPClient
 import RemoteConfig
 import Testing
 
-let apiKey = ProcessInfo.processInfo.environment["FIREBASE_API_TOKEN"]!
-let projectId = ProcessInfo.processInfo.environment["FIREBASE_PROJECT_ID"]!
-let projectName = ProcessInfo.processInfo.environment["FIREBASE_PROJECT_NAME"]!
-let appId = ProcessInfo.processInfo.environment["FIREBASE_APP_ID"]!
-
-let client = RemoteConfigClient(
-  apiKey: apiKey,
-  projectId: projectId,
-  projectName: projectName,
-  appId: appId,
-  appInstanceId: UUID().uuidString,
-  httpClient: .urlSession(.shared)
-)
-
-@Test
-func fetch() async throws {
-  let response = try await client.fetch()
-  #expect(response.entries.keys.contains("parameterBool"))
-  #expect(response.entries.keys.contains("parameterInt"))
-  #expect(response.entries.keys.contains("parameterString"))
-  #expect(response.entries.keys.contains("parameterJson"))
-}
-
-#if canImport(ObjectiveC)
-  @Test
-  func realtimeStream() async throws {
-    for try await result in client.realtimeStream() {
-      _ = try result.get()
-      let remoteConfig = try await client.fetch()
-      #expect(remoteConfig.entries.keys.contains("parameterBool"))
-      #expect(remoteConfig.entries.keys.contains("parameterInt"))
-      #expect(remoteConfig.entries.keys.contains("parameterString"))
-      #expect(remoteConfig.entries.keys.contains("parameterJson"))
-    }
-  }
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
 #endif
+
+@Suite
+struct RemoteConfigTests {
+  let client: RemoteConfigClient<URLSession>
+
+  init() {
+    let apiKey = ProcessInfo.processInfo.environment["FIREBASE_API_TOKEN"]!
+    let projectId = ProcessInfo.processInfo.environment["FIREBASE_PROJECT_ID"]!
+    let projectName = ProcessInfo.processInfo.environment["FIREBASE_PROJECT_NAME"]!
+    let appId = ProcessInfo.processInfo.environment["FIREBASE_APP_ID"]!
+
+    self.client = RemoteConfigClient(
+      apiKey: apiKey,
+      projectId: projectId,
+      projectName: projectName,
+      appId: appId,
+      appInstanceId: UUID().uuidString,
+      httpClient: .urlSession(.shared)
+    )
+  }
+
+  @Test
+  func fetch() async throws {
+    let response = try await client.fetch()
+    #expect(response.entries.keys.contains("parameterBool"))
+    #expect(response.entries.keys.contains("parameterInt"))
+    #expect(response.entries.keys.contains("parameterString"))
+    #expect(response.entries.keys.contains("parameterJson"))
+  }
+
+  #if canImport(ObjectiveC)
+    @Test
+    func realtimeStream() async throws {
+      for try await result in client.realtimeStream() {
+        _ = try result.get()
+        let remoteConfig = try await client.fetch()
+        #expect(remoteConfig.entries.keys.contains("parameterBool"))
+        #expect(remoteConfig.entries.keys.contains("parameterInt"))
+        #expect(remoteConfig.entries.keys.contains("parameterString"))
+        #expect(remoteConfig.entries.keys.contains("parameterJson"))
+      }
+    }
+  #endif
+}
